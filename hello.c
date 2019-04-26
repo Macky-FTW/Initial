f63c5b7ee0fc0b8eafd3a78b12db9287415db63e
Kyle McGilvray <c3329160@uon.edu.au>
2019-04-26 02:29:59 +0000

    Nearly finished,

--- hello.c
+++ hello.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
-#include <math.h>
+#include<strings.h>
 
-int main()
+//Function prototypes / substitution cipher strings
+
+char *Cencryption(char[]);
+char *Cdecryption(char[]);
+char *Sencryption(char []);
+char *Sdecryption(char []);
+char alphabet[26] = {'A', 'B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char KEY[26];
+
+int main ()
 {
-    char message[999];
-	int Cipher;
+    int Cipher;
 	int Task;
-	int x;
-	int key;
-	int i;
-	//Heading
+	char text[999];
+    char *c_text;
+
+    	//Heading
 
 	printf("\t\t Cipher Algorithms\n");
 	printf("\t\t -----------------\n");
 
-	//Cipher selection menu between rotational (caesar) and substitution ciphers
+		//Cipher type selection menu between rotational (caesar) and substitution ciphers
 
 	printf("\t************************************\n");
 	printf("\t* Cipher Types:                    *\n");
@@ -23,153 +31,323 @@ int main()
 	printf("\t* 2) Substitution Cipher           *\n");
 	printf("\t************************************\n");
 
-	//Cipher type selection
+		//User enters "1" or "2" to select cipher type
 
 	printf("\n\t - Select Cipher Number: ");
 	scanf("%d", &Cipher);
-	 
-	//Cipher selection determins which cipher type is chosen. 'If' (Caesar) 'else if' (substitution) and 'else' (non-specifed Cipher number)
+    printf("\n\t***********************************************\n");
 
-	if (Cipher == 1)
+
+		//Cipher selection determins which cipher type is chosen. 'if' (Caesar) 'else if' (substitution) and 'else' (non-specifed Cipher selection)
+
+
+    	if (Cipher == 1)
 	{
-		//Cipher selection
+		//Cipher selection is printed to screen
 
-		printf("\n\n\t\t Rotation (Caesar) Cipher\n");
-		printf("\t\t---------------------------\n");
+		printf("\n\t\t Rotation (Caesar) Cipher\n");
+		printf("\t\t ------------------------\n");
+
 		//Cipher task menu
 
-		printf("\t****************************************************\n");
-		printf("\t* Tasks:                                           *\n");
-		printf("\t* ------                                           *\n");
-		printf("\t* 1) Encryption: (given algorithm/text/key)        *\n");
-		printf("\t* 2) Decryption: (given algorithm/cipher text/key) *\n");
-		printf("\t* 3) Decryption: (given cipher text/assumptions)   *\n");
-		printf("\t****************************************************\n");
+		printf("\t***********************************************\n");
+		printf("\t* Tasks:                                      *\n");
+		printf("\t* ------                                      *\n");
+		printf("\t* 1) Encryption: (given plain text/key)       *\n");
+		printf("\t* 2) Decryption: (given cipher text/key)      *\n");
+		printf("\t***********************************************\n");
 
 		//Task selection
 
 		printf("\n\t - Select Task Number: ");
 		scanf("%d", &Task);
 
-		//Task selection determins which case is chosen. 'Case 0:' (Task 1) 'case 1:' (Task 2) 'case 2:' (Task 3) and 'default' (any non-specified Task number)
+		//Task selection determins which case is chosen. 'Case 0:' (Task 1), 'case 1:' (Task 2) and 'default' (any non-specified Task selection)
 
 		switch (Task - 1)
 		{
 		case 0:
-
-			printf("\n\t - Selection: Encryption: (given algorithm/text/key)\n");
-			x = 1;
+            c_text = Cencryption(text);
 			break;
+
 		case 1:
-
-			printf("\n\t - Selection: Decryption: (given algorithm/cipher text/key)\n");
-			x = 2;
-			break;
-		case 2:
-
-			printf("\n\t - Selection: Decryption: (given cipher text/assumptions)\n");
-			x = 3;
+            c_text = Cdecryption(text);
 			break;
 
 			//Default prevents user from implementing an unassigned task selection
 
 		default:
-			
-			x = 7;
+
+            //Unassigned selection prints this message
+
+            printf("\n\t - ERROR: Character Not Assigned!\n");
 			break;
 		}
 	}
 	else if (Cipher == 2)
 	{
-		//Cipher selection
+		//Cipher selection is printed to screen
 
-		printf("\n\n\t\t Substitution Cipher\n");
+		printf("\n\t\t Substitution Cipher\n");
 		printf("\t\t -------------------\n");
 
 		//Cipher task menu
 
-		printf("\t****************************************************\n");
-		printf("\t* Tasks:                                           *\n");
-		printf("\t* ------                                           *\n");
-		printf("\t* 1) Encryption: (given algorithm/text/key)        *\n");
-		printf("\t* 2) Decryption: (given algorithm/cipher text/key) *\n");
-		printf("\t* 3) Decryption: (given cipher text/assuptions)    *\n");
-		printf("\t****************************************************\n");
+		printf("\t***********************************************\n");
+		printf("\t* Tasks:                                      *\n");
+		printf("\t* ------                                      *\n");
+		printf("\t* 1) Encryption: (given plain text/key)       *\n");
+		printf("\t* 2) Decryption: (given cipher text/key)      *\n");
+		printf("\t***********************************************\n");
 
 		//Task selection
 
 		printf("\n\t - Select Task Number: ");
 		scanf("%d", &Task);
 
-		//Task selection determins which case is chosen. 'Case 0:' (Task 1) 'case 1:' (Task 2) 'case 2:' (Task 3) and 'default' (any non-specified Task number)
+		//Task selection determins which case is chosen. 'Case 0:' (Task 1), 'case 1:' (Task 2) and 'default' (any non-specified Task number)
 
 		switch (Task - 1)
 		{
 		case 0:
 
-			printf("\n\t - Selection: Encryption: (given algorithm/key/text)\n");
-			x = 4;
-			break;
+            //Calls the substitution encyption function
+
+            c_text = Sencryption(text);
+
+        break;
 		case 1:
 
-			printf("\n\t - Selection: Decryption: (given algorithm/cipher text/key)\n");
-			x = 5;
-			break;
-		case 2:
+            //Calls the substitution decryption function
 
-			printf("\n\t - Selection: Decryption: (given cipher text/assumptions)\n");
-			x = 6;
+            c_text = Sdecryption(text);
+
 			break;
 
 			//Default prevents user from implementing an unassigned task selection
 
 		default:
 
-			x = 7;
+            //Unassigned selection prints this message
+
+            printf("\n\t - ERROR: Character Not Assigned!\n");
 		}
-
-
 	}
+	//'else' prevents user from implementing an unassigned cipher type selection
 	else
 	{
-		//This prevents bugs and acts as a 'default' in a switch statement
-		x = 7;
-	}
-	switch (x - 1)
-	{
-	case 0:
-		printf("Enter Key: ");
-		scanf("%d", &key);
-		FILE *caesar1;
-		caesar1 = fopen("caesar1.txt", "r");
-		if (caesar1)
-		{
-		    while(fscanf (caesar1, "%c", &message[i]) != EOF)
-		    {
-		    printf("%c", message[i]);
-		    i++;
-		    fclose(caesar1);
-	        }
+	    //All non specified cipher values
 
-		}
-		break;
-	case 1:
-		printf("%d", x);
-		break;
-	case 2:
-		printf("%d", x);
-		break;
-	case 3:
-		printf("%d", x);
-		break;
-	case 4:
-		printf("%d", x);
-		break;
-	case 5:
-		printf("%d", x);
-		break;
-	default:
-		printf("\n\t - ERROR: Not Assigned To Task!\n");
+		printf("\tERROR: PLEASE SELECT ASSIGNED VALUE");
+		printf("\n\t***********************************************\n");
 	}
 	return 0;
+	}
+//Caesar encryption function definition
+char *Cencryption(char cipher_text[])
+{
+
+    int i;
+    int key;
+    char ch;
+    char text[999];
+    //Stating the task selection
+
+			printf("\n\t - Selection: Encryption\n");
+			printf("\n\t***********************************************************\n\n");
+
+			//User enters plain text. 'Tab' MUST be pressed before the 'Enter' to continue, in case plain text consists of paragraphs.
+
+			printf("\t - Enter Plain Text (press 'Tab' then 'Enter' to continue): ");
+			scanf("%[^\t]s", &text);
+
+			//User enters numbered key to rotate character values
+
+			printf("\n\n\t - Enter Key: ");
+			scanf("%d", &key);
+
+	//Ceasar cipher encryption algorithm
+	//Each character of the plain text is being tested at increment 'i'
+
+    for(i = 0; text[i] != '\0'; i++)
+    {
+
+    //the character at increment 'i' is being selected for testing
+
+    ch = text[i];
+
+        //Checks for lower case characters and coverts them to upper case by subtracting '32' from the characeter's ASCII value
+
+        if (ch >= 'a' && ch <= 'z')
+        {
+        ch = ch + key - 32;
+
+            //Keeps key confined to alphabetic characters only
+
+            if(ch > 'Z')
+            {
+            ch = ch - 'Z' + 'A' - 1;
+            }
+         text[i] = ch;
+        }
+
+        //Converts plain text into cipher text by adding the key value to the ASCII value of each upper case character
+
+        else if (ch >= 'A' && ch <= 'Z')
+        {
+        ch = ch + key;
+
+            //Keeps key confined to alphabetic characters only
+
+            if(ch > 'Z')
+            {
+            ch = ch - 'Z' + 'A' - 1;
+            }
+        text[i] = ch;
+        }
+    }
+
+    //Display of the encrypted cipher text
+
+    printf("\n\t***********************************************************\n\n");
+    printf("\t - Encryption: %s", text);
+    printf("\n\n\t***********************************************************\n");
+    return cipher_text;
+
 }
+//Caesar decryption function definition
+char *Cdecryption(char cipher_text[])
+{
+
+    int i;
+    int key;
+    char ch;
+    char text[999];
+
+    //Stating the task selection
+
+		    printf("\n\t - Selection: Decryption: (given cipher text/key)\n");
+            printf("\n\t************************************************************\n\n");
+
+            //User enters cipher text. 'Tab' MUST be pressed before the 'Enter' to continue, in case plain text consists of paragraphs.
+
+			printf("\t - Enter Cipher Text (press 'Tab' then 'Enter' to continue): ");
+			scanf("%[^\t]s", &text);
+
+			//User enters numbered key to rotate character values
+
+			printf("\n\n\t - Enter Key: ");
+			scanf("%d", &key);
+
+    //Ceasar cipher decryption algorithm
+	//Each character of the cipher text is being tested at increment 'i'
+
+    for(i = 0; text[i] != '\0'; ++i)
+    {
+
+        //the character at increment 'i' is being selected for testing
+
+    ch = text[i];
+
+        //Checks for lower case characters and coverts them to upper case by subtracting '32' from the characeter's ASCII value
+
+
+        if(ch >= 'a' && ch <= 'z')
+            {
+
+			ch = ch - key - 32;
+
+            //Keeps key confined to alphabetic characters only
+
+			if(ch < 'A')
+			{
+				ch = ch + 'Z' - 'A' + 1;
+			}
+            text[i] = ch;
+		}
+
+        //Converts cipher text into plain text by subtracting the key value to the ASCII value of each upper case character
+
+		else if(ch >= 'A' && ch <= 'Z')
+		{
+			ch = ch - key;
+
+            //Keeps key confined to alphabetic characters only
+
+			if(ch < 'A')
+			{
+				ch = ch + 'Z' - 'A' + 1;
+			}
+			text[i] = ch;
+		}
+	}
+
+    //Display of the decrypted plain text
+
+    printf("\n\t***********************************************************\n\n");
+	printf("\t - Decryption: %s", text);
+	printf("\n\n\t***********************************************************\n");
+	return cipher_text;
+
+}
+//Substitution encryption function definition
+
+char *Sencryption(char cipher_text[])
+{
+    int i;
+    int j;
+    char text[999];
+
+    //Stating the task selection
+
+            printf("\n\t - Selection: Encryption: (given plain text/key)\n");
+			printf("\n\t***********************************************************\n\n");
+
+			//User enters plain text. 'Tab' MUST be pressed before the 'Enter' to continue, in case plain text consists of paragraphs
+
+			printf("\t - Enter Plain Text (press 'Tab' then 'Enter' to continue): ");
+			scanf("%[^\t]s", text);
+
+			//User enters key. Must contain 26 upper case alphabetic characters in any order (each must be unique)
+
+			printf("\n\n\t - Enter Key Of 26 Upper Case Characters: ");
+			scanf("%s", KEY);
+
+			//Display of what each letter is assigned to in the key
+
+            printf("\n\tABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+            printf("\t||||||||||||||||||||||||||");
+            printf("\n\t%s\n", KEY);
+
+    //Each character of the plain text is being tested at increment 'i' while it is less than the stringlength
+
+    for(i = 0; i < strlen(cipher_text); i++)
+    {
+        //Each selected character at increment 'i' will be tested for its position in the alphabet and corresponding position in the key at increment 'j'
+
+        for(j = 0; j < 26; j++)
+        {
+            //If the character in the plain text and alphabet are the same, the character is then converted into the corresponding key character
+
+            if(alphabet[j] == cipher_text[i])
+            {
+
+             //Converting character into key associated character
+
+             cipher_text[i] = KEY[j];
+             break;
+            }
+        }
+    }
+
+    //Display of the encrypted/cipher text
+
+    printf("\n\t***********************************************************\n");
+    printf("\n\tEncryption: %s", cipher_text);
+    printf("\n\n\t***********************************************************\n");
+    return cipher_text;
+}
+//Substitution decryption function definition
+
+char *Sdecryption(char cipher_text[])
+{
